787e8c45ea65da5becafa33f00f6df06673dc1c8
Gururaghavendra s <gurugururaghavendras@gmail.com>
2026-07-22 16:43:49 +0530

    Add matrix addition program in C

---
+++ matrix.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int matrix_1[3][3] = {1,2,3,4,5,6,7,8,9};
+    int matrix_2[3][3] = {9,8,7,6,5,4,3,2,1};
+    
+    int sum[3][3];
+
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            sum[i][j] = matrix_1[i][j] + matrix_2[i][j];
+        }
+    }
+
+    printf("Result of matrix addition:\n");
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
