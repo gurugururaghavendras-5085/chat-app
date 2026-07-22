1faaa5e5644e7241d908bc3c09edb4f989d7e44c
Gururaghavendra s <gurugururaghavendras@gmail.com>
2026-07-22 16:41:51 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+num1=float(input("Enter first number:"))
+op=input("Enter operators(+,-,*,/)")
+num2=float(input("Enter second number:"))
+if op=="+":
+    print("Addition:",num1+num2)
+elif op=="-":
+    print("subtraction:",num1-num2)
+elif op=="*":
+    print("Multiply:",num1*num2)
+elif op=="/":
+    print("Division:",num1/num2)
---
+++ 2.py
@@ -0,0 +1,4 @@
+list=[-2,-1,0,1,2,3,-4,5]
+li=list(filter(lambda X:X>0),list)
+print("Positive numbers")
+print("original".list)
